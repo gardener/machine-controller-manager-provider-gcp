ce6eb9ed44859865e4cabafb393d968ce1f5eb39
Himanshu Sharma <79965161+himanshu-kun@users.noreply.github.com>
2023-05-24 14:16:24 +0530

    Revendor mcm v0.49 (#80)
    * adapted to breaking changes
    
    * vendored files
    
    * update mcm v0.49.1

--- pkg/gcp/machine_controller_test.go
+++ pkg/gcp/machine_controller_test.go
@@ -81,8 +81,6 @@ var _ = Describe("#MachineController", func() {
 	gcpProviderSpecNoTagsToSearch := []byte("{\"canIpForward\":true,\"deletionProtection\":false,\"description\":\"Machine created to test out-of-tree gcp mcm driver.\",\"disks\":[{\"autoDelete\":true,\"boot\":true,\"sizeGb\":50,\"type\":\"pd-standard\",\"image\":\"projects/coreos-cloud/global/images/coreos-stable-2135-6-0-v20190801\",\"labels\":{\"name\":\"test-mc-gcp\"}}],\"labels\":{\"name\":\"test-mc-gcp\"},\"machineType\":\"n1-standard-2\",\"metadata\":[{\"key\":\"gcp\",\"value\":\"my-value\"}],\"networkInterfaces\":[{\"network\":\"dummyShoot\",\"subnetwork\":\"dummyShoot\"}],\"scheduling\":{\"automaticRestart\":true,\"onHostMaintenance\":\"MIGRATE\",\"preemptible\":false},\"secretRef\":{\"name\":\"dummySecret\",\"namespace\":\"dummy\"},\"serviceAccounts\":[{\"email\":\"mcmDummy@dummy.com\",\"scopes\":[\"https://www.googleapis.com/auth/compute\"]}],\"region\":\"europe-dummy\",\"zone\":\"europe-dummy\"}")
 	gcpProviderSpecInvalidPostZone := []byte("{\"canIpForward\":true,\"deletionProtection\":false,\"description\":\"Machine created to test out-of-tree gcp mcm driver.\",\"disks\":[{\"autoDelete\":true,\"boot\":true,\"sizeGb\":50,\"type\":\"pd-standard\",\"image\":\"projects/coreos-cloud/global/images/coreos-stable-2135-6-0-v20190801\",\"labels\":{\"name\":\"test-mc-gcp\"}}],\"labels\":{\"name\":\"test-mc-gcp\"},\"machineType\":\"n1-standard-2\",\"metadata\":[{\"key\":\"gcp\",\"value\":\"my-value\"}],\"networkInterfaces\":[{\"network\":\"dummyShoot\",\"subnetwork\":\"dummyShoot\"}],\"scheduling\":{\"automaticRestart\":true,\"onHostMaintenance\":\"MIGRATE\",\"preemptible\":false},\"secretRef\":{\"name\":\"dummySecret\",\"namespace\":\"dummy\"},\"serviceAccounts\":[{\"email\":\"mcmDummy@dummy.com\",\"scopes\":[\"https://www.googleapis.com/auth/compute\"]}],\"tags\":[\"kubernetes-io-cluster-dummy-machine\",\"kubernetes-io-role-mcm\",\"dummy-machine\"],\"region\":\"europe-dummy\",\"zone\":\"invalid post\"}")
 	gcpProviderSpecInvalidListZone := []byte("{\"canIpForward\":true,\"deletionProtection\":false,\"description\":\"Machine created to test out-of-tree gcp mcm driver.\",\"disks\":[{\"autoDelete\":true,\"boot\":true,\"sizeGb\":50,\"type\":\"pd-standard\",\"image\":\"projects/coreos-cloud/global/images/coreos-stable-2135-6-0-v20190801\",\"labels\":{\"name\":\"test-mc-gcp\"}}],\"labels\":{\"name\":\"test-mc-gcp\"},\"machineType\":\"n1-standard-2\",\"metadata\":[{\"key\":\"gcp\",\"value\":\"my-value\"}],\"networkInterfaces\":[{\"network\":\"dummyShoot\",\"subnetwork\":\"dummyShoot\"}],\"scheduling\":{\"automaticRestart\":true,\"onHostMaintenance\":\"MIGRATE\",\"preemptible\":false},\"secretRef\":{\"name\":\"dummySecret\",\"namespace\":\"dummy\"},\"serviceAccounts\":[{\"email\":\"mcmDummy@dummy.com\",\"scopes\":[\"https://www.googleapis.com/auth/compute\"]}],\"tags\":[\"kubernetes-io-cluster-dummy-machine\",\"kubernetes-io-role-mcm\",\"dummy-machine\"],\"region\":\"europe-dummy\",\"zone\":\"invalid list\"}")
-	gcpProviderSpecExpectedAfterMigration := []byte("{\"APIVersion\":\"mcm.gardener.cloud/v1alpha1\",\"canIpForward\":true,\"deletionProtection\":false,\"description\":\"Machine created to test out-of-tree gcp mcm driver.\",\"disks\":[{\"autoDelete\":true,\"boot\":true,\"sizeGb\":50,\"type\":\"pd-standard\",\"interface\":\"test-interface\",\"image\":\"projects/coreos-cloud/global/images/coreos-stable-2135-6-0-v20190801\",\"labels\":{\"name\":\"test-mc-gcp\"}}],\"labels\":{\"name\":\"test-mc-gcp\"},\"machineType\":\"n1-standard-2\",\"metadata\":[{\"key\":\"gcp\",\"value\":\"my-value\"}],\"networkInterfaces\":[{\"network\":\"dummyShoot\",\"subnetwork\":\"dummyShoot\"}],\"region\":\"europe-dummy\",\"scheduling\":{\"automaticRestart\":true,\"onHostMaintenance\":\"MIGRATE\",\"preemptible\":false},\"serviceAccounts\":[{\"email\":\"mcmDummy@dummy.com\",\"scopes\":[\"https://www.googleapis.com/auth/compute\"]}],\"tags\":[\"kubernetes-io-cluster-dummy-machine\",\"kubernetes-io-role-mcm\",\"dummy-machine\"],\"zone\":\"europe-dummy\"}")
-
 	gcpPVSpecIntree := &corev1.PersistentVolumeSpec{
 		PersistentVolumeSource: corev1.PersistentVolumeSource{
 			GCEPersistentDisk: &corev1.GCEPersistentDiskVolumeSource{
@@ -123,7 +121,7 @@ var _ = Describe("#MachineController", func() {
 	}
 
 	gcpProviderSecretWithMisssingUserData := map[string][]byte{
-		//"userData":           []byte(""),
+		// "userData":           []byte(""),
 		api.GCPServiceAccountJSON: []byte("{\"type\":\"service_account\",\"project_id\":\"sap-se-gcp-scp-k8s-dev\"}"),
 	}
 	gcpProviderSecretWithoutProjectID := map[string][]byte{
@@ -135,8 +133,8 @@ var _ = Describe("#MachineController", func() {
 	var mockPluginSPIImpl *fake.PluginSPIImpl
 
 	var _ = BeforeSuite(func() {
-		//Start a mock server to listen to mock client requests
-		//This is rquired as compute sdk doesn't offer any interface so the mocking is done via a mock http client pass to the compute service
+		// Start a mock server to listen to mock client requests
+		// This is rquired as compute sdk doesn't offer any interface so the mocking is done via a mock http client pass to the compute service
 		go fake.NewMockServer()
 		mockPluginSPIImpl = &fake.PluginSPIImpl{Client: &http.Client{}}
 		ms = NewGCPPlugin(mockPluginSPIImpl)
@@ -761,150 +759,6 @@ var _ = Describe("#MachineController", func() {
 			}),
 		)
 	})
-	Describe("##GenerateMachineClassForMigration", func() {
-		type setup struct {
-		}
-		type action struct {
-			machineRequest *driver.GenerateMachineClassForMigrationRequest
-		}
-		type expect struct {
-			machineClass *v1alpha1.MachineClass
-			err          error
-		}
-		type data struct {
-			setup  setup
-			action action
-			expect expect
-		}
-
-		DescribeTable("###table",
-			func(data *data) {
-
-				ctx := context.Background()
-				_, err := ms.GenerateMachineClassForMigration(ctx, data.action.machineRequest)
-				if data.expect.err != nil {
-					Expect(err).To(HaveOccurred())
-					Expect(err).To(Equal(err))
-				} else {
-					Expect(err).ToNot(HaveOccurred())
-					Expect(data.action.machineRequest.MachineClass.ProviderSpec).To(Equal(data.expect.machineClass.ProviderSpec))
-					Expect(data.action.machineRequest.MachineClass.Provider).To(Equal(ProviderGCP))
-					Expect(data.action.machineRequest.MachineClass.SecretRef).To(Equal(data.expect.machineClass.SecretRef))
-					Expect(data.action.machineRequest.MachineClass.CredentialsSecretRef).To(Equal(data.expect.machineClass.CredentialsSecretRef))
-				}
-			},
-
-			Entry("With valid migration request", &data{
-				setup: setup{},
-				action: action{
-					machineRequest: &driver.GenerateMachineClassForMigrationRequest{
-						ProviderSpecificMachineClass: &v1alpha1.GCPMachineClass{
-							ObjectMeta: metav1.ObjectMeta{
-								Name:      TestMachineClassName,
-								Namespace: TestNamaspace,
-							},
-							Spec: v1alpha1.GCPMachineClassSpec{
-								CanIpForward:       true,
-								DeletionProtection: false,
-								Description:        getStringPtr("Machine created to test out-of-tree gcp mcm driver."),
-								Disks: []*v1alpha1.GCPDisk{
-									{
-										AutoDelete: getBoolPtr(true),
-										Boot:       true,
-										SizeGb:     50,
-										Type:       "pd-standard",
-										Interface:  "test-interface",
-										Image:      "projects/coreos-cloud/global/images/coreos-stable-2135-6-0-v20190801",
-										Labels: map[string]string{
-											"name": "test-mc-gcp",
-										},
-									},
-								},
-								Labels: map[string]string{
-									"name": "test-mc-gcp",
-								},
-								MachineType: "n1-standard-2",
-								Metadata: []*v1alpha1.GCPMetadata{
-									{
-										Key:   "gcp",
-										Value: getStringPtr("my-value"),
-									},
-								},
-								NetworkInterfaces: []*v1alpha1.GCPNetworkInterface{
-									{
-										DisableExternalIP: false,
-										Network:           "dummyShoot",
-										Subnetwork:        "dummyShoot",
-									},
-								},
-								Scheduling: v1alpha1.GCPScheduling{
-									AutomaticRestart:  true,
-									OnHostMaintenance: "MIGRATE",
-									Preemptible:       false,
-								},
-								SecretRef: &corev1.SecretReference{
-									Name:      "test-secret",
-									Namespace: TestNamaspace,
-								},
-								CredentialsSecretRef: &corev1.SecretReference{
-									Name:      "test-credential",
-									Namespace: TestNamaspace,
-								},
-								ServiceAccounts: []v1alpha1.GCPServiceAccount{
-									{
-										Email: "mcmDummy@dummy.com",
-										Scopes: []string{
-											"https://www.googleapis.com/auth/compute",
-										},
-									},
-								},
-								Tags: []string{
-									"kubernetes-io-cluster-dummy-machine",
-									"kubernetes-io-role-mcm",
-									"dummy-machine",
-								},
-								Region: "europe-dummy",
-								Zone:   "europe-dummy",
-							},
-						},
-						MachineClass: &v1alpha1.MachineClass{
-							ObjectMeta: metav1.ObjectMeta{
-								Name:      TestMachineClassName,
-								Namespace: TestNamaspace,
-							},
-							Provider: ProviderGCP,
-						},
-						ClassSpec: &v1alpha1.ClassSpec{
-							Kind: GCPMachineClassKind,
-							Name: TestMachineClassName,
-						},
-					},
-				},
-				expect: expect{
-					machineClass: &v1alpha1.MachineClass{
-						ObjectMeta: metav1.ObjectMeta{
-							Name:      TestMachineClassName,
-							Namespace: TestNamaspace,
-						},
-						ProviderSpec: runtime.RawExtension{
-							Raw:    gcpProviderSpecExpectedAfterMigration,
-							Object: nil,
-						},
-						SecretRef: &corev1.SecretReference{
-							Name:      "test-secret",
-							Namespace: TestNamaspace,
-						},
-						CredentialsSecretRef: &corev1.SecretReference{
-							Name:      "test-credential",
-							Namespace: TestNamaspace,
-						},
-						Provider: ProviderGCP,
-					},
-				},
-			}),
-		)
-	})
-
 })
 
 func getBoolPtr(b bool) *bool {
