00dd3f396c0c2d814e36d1c2f50e4e69efbd6b89
Rishabh Patel <rishabh.patel@sap.com>
2023-06-08 10:58:02 +0530

    update generate-spec-doc.sh

--- hack/api-reference/generate-spec-doc.sh
+++ hack/api-reference/generate-spec-doc.sh
@@ -1,5 +1,7 @@
+cd ./hack/api-reference
 ./gen-crd-api-reference-docs -config "providerspec-config.json" -api-dir "../../pkg/api/v1alpha1" -out-file="../../docs/docs/provider-spec.md"
-
+sed 's/?id=//g' ../../docs/docs/provider-spec.md > ../../docs/docs/provider-spec-1.md
+mv ../../docs/docs/provider-spec-1.md ../../docs/docs/provider-spec.md
 
 
 
